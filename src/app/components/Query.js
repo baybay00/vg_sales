13dd11fe5d49e15b843acd5ce4d5ba30a00381b4
Bailey <baileyg3010@gmail.com>
2024-04-20 10:14:05 -0400

    home page done

---
+++ src/app/components/Query.js
@@ -0,0 +1,41 @@
+'use client';
+
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+
+const Query = (props) => {
+
+    const router = useRouter()
+    const [enteredQuery, setEnteredQuery] = useState('')
+
+    const queryHandler = (e) => {
+        setEnteredQuery(e.target.value)
+    }
+
+    const submitHandler = (e) => {
+        e.preventDefault()
+
+        const query = {
+            query: enteredQuery
+        }
+        props.onQuery(query)
+        router.push('/query')
+    }
+
+    return (
+        <div className="flex flex-col items-center justify-center h-screen bg-navy text-white">
+            <p className="p-2">Please enter a query.</p>
+            <form className="flex flex-col items-center" onSubmit={submitHandler}>
+                <input
+                    type="text"
+                    value={enteredQuery}
+                    onChange={queryHandler}
+                    className="m-2 p-2 rounded-lg border border-white h-48 w-96"
+                />
+                <button type="submit" className="m-2 p-2 bg-white text-navy rounded-lg">Submit</button>
+            </form>
+        </div>
+    )
+}
+
+export default Query;
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,12 @@ module.exports = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        'navy': '#153448',
+        'blue': '#3C5B6F',
+        'tan': '#948979',
+        'beige': '#DFD0B8',
+      }
     },
   },
   plugins: [],
